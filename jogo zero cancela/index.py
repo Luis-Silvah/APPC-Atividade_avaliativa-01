3ba90bb5fa7b465d57f62c8fcc317cec7d9c3218
Luis Silvah <luisgsilva21@gmail.com>
2024-04-03 23:38:10 -0300

    Melhorando a performace do codigo

--- jogo zero cancela/index.py
+++ jogo zero cancela/index.py
@@ -1,45 +1,73 @@
-num=float(input('\n\t Digite um numero para somar: '))
+print(
+    "\n\t",
+    33 * "=",
+    "\n\t | \t BEM VINDO! \t\t |",
+    "\n\t | \t JOGO ZERO CANCELA \t |",
+    "\n\t",
+    33 * "=",
+)
+
+num = float(input("\n\t Digite um número para somar: "))
 
 soma = 0
 
-contRodadas = 0
+contRodadasZero = 0
 
 contDesconsiderados = 0
 contConsiderados = 0
 
-subtrair = 0
-
-num01 = 0
-num02 = 0
-num03 = 0
+numAntecessor01 = 0
+numAntecessor02 = 0
+numAntecessor03 = 0
 
 while num >= 0:
-    if (num == 0): 
-        print(f"Subtrair Número  {subtrair}")
-        contDesconsiderados += 1
-        if contDesconsiderados >= 3:
-           print(f"Só é permitido até 3 zeros consecutivos!!!") 
+    if num == 0:
+        contRodadasZero += 1
+
+        # if contRodadasZero == 1:
+        #     contDesconsiderados += 1
+        #     contConsiderados -= 1
+
+        # elif contRodadasZero == 2:
+        #     soma -= numAntecessor01
+        #     contDesconsiderados += 1
+        #     contConsiderados -= 1
+
+        # elif contRodadasZero == 3:
+        #     soma -= numAntecessor01
+        #     contDesconsiderados += 1
+        #     contConsiderados -= 1
+
+        if contRodadasZero > 3:
+            print(f"Só é permitido até 3 zeros consecutivos!!!")
         else:
-            print(f"Subtrair Número {contDesconsiderados} {subtrair}")
-        
-    else: 
-        subtrair = num
-        soma = soma + num
+            soma -= numAntecessor01
+            contConsiderados -= 1
+            contDesconsiderados += 1
+
+        numAntecessor01 = numAntecessor02
+        numAntecessor02 = numAntecessor03
+        numAntecessor03 = 0
+
+    else:
+        soma += num
         contConsiderados += 1
-       
-    contRodadas += 1
-
-    print(f"contador de rodadas: {contRodadas}")
-
-    if(contRodadas < 4):
-        print(f"Rodada {num}")
-    else: 
-        contRodadas = 1
-
-    num=float(input('\n\t Digite um numero para somar: '))
-
+        contRodadasZero = 0
         
-print(f"\n Números desconsideraos = {contDesconsiderados}")
-print(f"\n Números considerados = {contConsiderados}")
-print(f"\n Soma Total = {soma}")
+        numAntecessor03 = numAntecessor02
+        numAntecessor02 = numAntecessor01
+        numAntecessor01 = num
 
+    num = float(input("\n\t Digite um número para somar: "))
+
+
+print(
+    "\n",
+    97 * "=",
+    "\n | \t SOMA TOTAL \t | \t Números considerados \t | \t Números desconsiderados \t |",
+    f"\n | \t  {soma} \t\t | \t {contConsiderados} \t\t\t | \t {contDesconsiderados} \t\t\t\t |",
+    "\n",
+    97 * "=",
+)
+
+print("\n\t", 33 * "-", "\n\t | \t ORBIGADO POR JOGAR! \t |", "\n\t", 33 * "-")
